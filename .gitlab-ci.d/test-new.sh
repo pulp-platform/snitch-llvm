88ed3e5e7ef9e8732a5ac7f87c566d8126a39aec
Noah Hütter <huettern@student.ethz.ch>
2021-09-03 06:36:28 +0000

    [CI] Add gitlab CI for ETHZ iis-git
    (cherry picked from commit 732ef48e04ab0a3fc28d5e2fddb6a6e6dd586398)

---
+++ .gitlab-ci.d/test-new.sh
@@ -0,0 +1,41 @@
+#!/bin/env bash
+# Copyright 2021 ETH Zurich and University of Bologna.
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Author: Noah Huetter <huettern@iis.ee.ethz.ch>, ETH Zurich
+
+# Arguments as environment variables:
+#  mergebase    to which revision to compare for new tests
+#  WORK         working directory with build LLVM
+
+set -ex
+
+old_pwd=${PWD}; cd ${WORK}
+
+if [ -z ${mergebase+x} ]; then
+    mergebase=llvmorg-12.0.1
+fi
+
+# Test discovery
+prefix=$(git rev-parse --show-toplevel)
+clang_tests=$(git diff --name-only $mergebase HEAD ${prefix}/clang/test | xargs -I{} -n1 echo ${prefix}/{})
+llvm_tests=$(git diff --name-only $mergebase HEAD ${prefix}/llvm/test | xargs -I{} -n1 echo ${prefix}/{})
+
+# Collect all tests and remove configs
+tests="${clang_tests} ${llvm_tests}"
+tests=$(echo "${tests}" | grep -vE 'lit\.local\.cfg|lit\.cfg\.py|Inputs')
+echo "-- Discovered tests:"
+for f in ${tests}; do
+    echo $f
+done
+echo "--------------------"
+
+# Run tests
+llvm/bin/llvm-lit -v --debug --no-indirectly-run-check ${tests}
+# lit -v --debug --no-indirectly-run-check ${tests}
+ret=$?
+
+cd ${old_pwd}
+exit ${ret}
---
+++ .gitlab/merge_request_templates/Default.md
@@ -0,0 +1,21 @@
+# Pull Request Template
+
+## Description
+
+
+## Type of change
+
+- [ ] Bug fix (non-breaking change which fixes an issue)
+- [ ] New feature (non-breaking change which adds functionality)
+- [ ] Breaking change (fix or feature that would cause existing functionality to not work as expected)
+- [ ] This change requires a documentation update
+
+## How Has This Been Tested?
+
+- [ ] 
+
+## Checklist:
+
+- [ ] Run all LLVM regression tests `ninja check-all`
+- [ ] Regression tests [added to the CI](https://github.com/pulp-platform/snitch-toolchain-cd/blob/main/stages/test-snitch-llvm.sh)? 
+- [ ] Major new features [added to the README](../README.md)

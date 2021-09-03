732ef48e04ab0a3fc28d5e2fddb6a6e6dd586398
Noah Hütter <huettern@student.ethz.ch>
2021-09-03 06:36:28 +0000

    [CI] Add gitlab CI for ETHZ iis-git

---
+++ .gitlab-ci.d/env-iis.mk
@@ -0,0 +1,11 @@
+# Copyright 2021 ETH Zurich and University of Bologna.
+# Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
+# See https://llvm.org/LICENSE.txt for license information.
+# SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
+
+# Host compilers to use
+CXX=/usr/sepp/bin/g++-9.2.0
+CC=/usr/sepp/bin/gcc-9.2.0
+
+# Utilities
+CMAKE=cmake-3.18.1
